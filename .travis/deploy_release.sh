07bffd049d23bfefc3dfddf272644aeb9ce709cd
Rami Sabbagh <ramilego4game@gmail.com>
2019-05-25 21:56:33 +0300

    Add butler support

--- .travis/deploy_release.sh
+++ .travis/deploy_release.sh
@@ -71,26 +71,30 @@ gothub upload \
   --user RexcellentGames \
   --repo CurseOfTheArrow \
   --tag $TRAVIS_TAG \
-  --name "CurseOfTheArrow-V"$TRAVIS_TAG"-Universal.love" \
-  --file ../BuildUtils/Builds/CurseOfTheArrow-Universal.love \
+  --name "CurseOfTheArrow-V"$TRAVIS_TAG"-universal.love" \
+  --file ../BuildUtils/Builds/CurseOfTheArrow-universal.love \
   --replace
 
-#echo -----------------------------------------
-#echo ----==== Deploying To itch.io ====----
-#echo -----------------------------------------
-#echo 
-#echo ----==== Renaming the builds ====----
+echo -----------------------------------------
+echo ----==== Deploying To itch.io ====----
+echo -----------------------------------------
+echo 
+echo ----==== Renaming the builds ====----
 
-#mv -v -f "../BuildUtils/Builds/CurseOfTheArrow_Nightly_Windows.zip" "../BuildUtils/Builds/CurseOfTheArrow_V"$TRAVIS_TAG"_Windows.zip"
-#mv -v -f "../BuildUtils/Builds/CurseOfTheArrow_Nightly_Linux_x86_64.AppImage" "../BuildUtils/Builds/CurseOfTheArrow_V"$TRAVIS_TAG"_Linux_x86_64.AppImage"
-#mv -v -f "../BuildUtils/Builds/CurseOfTheArrow_Nightly_Mac.zip" "../BuildUtils/Builds/CurseOfTheArrow_V"$TRAVIS_TAG"_Mac.zip"
-#mv -v -f "../BuildUtils/Builds/CurseOfTheArrow_Nightly_Universal.love" "../BuildUtils/Builds/CurseOfTheArrow_V"$TRAVIS_TAG"_Universal.love"
+mv -v -f "../BuildUtils/Builds/CurseOfTheArrow-win32.zip" "../BuildUtils/Builds/CurseOfTheArrow-V"$TRAVIS_TAG"-win32.zip"
+mv -v -f "../BuildUtils/Builds/CurseOfTheArrow-win64.zip" "../BuildUtils/Builds/CurseOfTheArrow-V"$TRAVIS_TAG"-win64.zip"
+mv -v -f "../BuildUtils/Builds/CurseOfTheArrow-linux-x86_64.zip" "../BuildUtils/Builds/CurseOfTheArrow-V"$TRAVIS_TAG"-linux-x86_64.zip"
+mv -v -f "../BuildUtils/Builds/CurseOfTheArrow-linux-i686.zip" "../BuildUtils/Builds/CurseOfTheArrow-V"$TRAVIS_TAG"-linux-i686.zip"
+mv -v -f "../BuildUtils/Builds/CurseOfTheArrow-macos.zip" "../BuildUtils/Builds/CurseOfTheArrow-V"$TRAVIS_TAG"-macos.zip"
+mv -v -f "../BuildUtils/Builds/CurseOfTheArrow-universal.love" "../BuildUtils/Builds/CurseOfTheArrow-V"$TRAVIS_TAG"-universal.love"
 
-#echo ----=== Uploading the builds ====----
+echo ----=== Uploading the builds ====----
 
-#butler push "../BuildUtils/Builds/CurseOfTheArrow_V"$TRAVIS_TAG"_Windows.zip" ramilego4game/liko12:windows
-#butler push "../BuildUtils/Builds/CurseOfTheArrow_V"$TRAVIS_TAG"_Linux_x86_64.AppImage" ramilego4game/liko12:linux
-#butler push "../BuildUtils/Builds/CurseOfTheArrow_V"$TRAVIS_TAG"_Mac.zip" ramilego4game/liko12:osx
-#butler push "../BuildUtils/Builds/CurseOfTheArrow_V"$TRAVIS_TAG"_Universal.love" ramilego4game/liko12:src
+butler push "../BuildUtils/Builds/CurseOfTheArrow-V"$TRAVIS_TAG"-win32.zip" egordorichev/curse-of-the-arrow:win32 --userversion $TRAVIS_TAG
+butler push "../BuildUtils/Builds/CurseOfTheArrow-V"$TRAVIS_TAG"-win64.zip" egordorichev/curse-of-the-arrow:win64 --userversion $TRAVIS_TAG
+butler push "../BuildUtils/Builds/CurseOfTheArrow-V"$TRAVIS_TAG"-linux-x86_64.zip" egordorichev/curse-of-the-arrow:linux-x86_64 --userversion $TRAVIS_TAG
+butler push "../BuildUtils/Builds/CurseOfTheArrow-V"$TRAVIS_TAG"-linux-i686.zip" egordorichev/curse-of-the-arrow:linux-i686 --userversion $TRAVIS_TAG
+butler push "../BuildUtils/Builds/CurseOfTheArrow-V"$TRAVIS_TAG"-macos.zip" egordorichev/curse-of-the-arrow:macos --userversion $TRAVIS_TAG
+butler push "../BuildUtils/Builds/CurseOfTheArrow-V"$TRAVIS_TAG"-universal.love" egordorichev/curse-of-the-arrow:src --userversion $TRAVIS_TAG
 
 echo ----==== Done ====----
