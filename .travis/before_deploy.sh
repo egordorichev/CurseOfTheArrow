aa80c7e4963986cd15a665375318e215c7ba5648
Rami Sabbagh <ramilego4game@gmail.com>
2019-05-25 16:37:52 +0300

    Typofixes

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -23,7 +23,7 @@ echo ----==== Building Windows 32bit ====----
 mv -v -f "./win32/CurseOfTheArrow.exe" "./win32/love.exe"
 cat win32/love.exe CurseOfTheArrow.love > win32/CurseOfTheArrow.exe
 rm -v -f win32/love.exe
-7z A -r -y -tzip -bd Builds/CurseOfTheArrow_win32.zip win32/*
+7z A -r -y -tzip -bd Builds/CurseOfTheArrow-win32.zip win32/*
 
 echo ----==== Building Windows 64bit ====----
 
@@ -31,31 +31,31 @@ echo ----==== Building Windows 64bit ====----
 mv -v -f "./win64/CurseOfTheArrow.exe" "./win64/love.exe"
 cat win64/love.exe CurseOfTheArrow.love > win64/CurseOfTheArrow.exe
 rm -v -f win64/love.exe
-7z A -r -y -tzip -bd Builds/CurseOfTheArrow_win64.zip win64/*
+7z A -r -y -tzip -bd Builds/CurseOfTheArrow-win64.zip win64/*
 
 echo ----==== Building Linux x86_64 ====----
 
 7z X -r -y -tzip -bd CurseOfTheArrow-linux-x86_64.zip  -o./linux-x86_64
 cp -v -f -r ./CurseOfTheArrow/* ./linux-x86_64/
 chmod a+x ./linux-x86_64/run.sh
-7z A -r -y -tzip -bd Builds/CurseOfTheArrow_linux-x86_64.zip linux-x86_64/*
+7z A -r -y -tzip -bd Builds/CurseOfTheArrow-linux-x86_64.zip linux-x86_64/*
 
 echo ----==== Building Linux i686 ====----
 
 7z X -r -y -tzip -bd CurseOfTheArrow-linux-i686.zip  -o./linux-i686
 cp -v -f -r ./CurseOfTheArrow/* ./linux-i686/
 chmod a+x ./linux-i686/run.sh
-7z A -r -y -tzip -bd Builds/CurseOfTheArrow_linux-i686.zip linux-i686/*
+7z A -r -y -tzip -bd Builds/CurseOfTheArrow-linux-i686.zip linux-i686/*
 
-echo ----==== Building OS_X ====----
+echo ----==== Building MacOS ====----
 
 7z X -r -y -tzip -bd CurseOfTheArrow-macos.zip  -o./macos
 cp -v -f CurseOfTheArrow.love macos/CurseOfTheArrow.app/Contents/Resources/CurseOfTheArrow.love
-7z A -r -y -tzip -bd Builds/CurseOfTheArrow_macos.zip macos/*
+7z A -r -y -tzip -bd Builds/CurseOfTheArrow-macos.zip macos/*
 
 echo ----==== Copying Universal... ====----
   
-cp -v -f CurseOfTheArrow.love Builds/CurseOfTheArrow_Universal.love
+cp -v -f CurseOfTheArrow.love Builds/CurseOfTheArrow-Universal.love
 
 echo ----==== Downloading Deployment Tools ====----
 
